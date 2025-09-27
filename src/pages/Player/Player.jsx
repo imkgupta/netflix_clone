e315ec4373606e8d1ed6c09c00fd8412d8e9290d
imkgupta <ketangupta889@gmail.com>
2025-09-27 22:41:42 +0530

    initial commit with netflix_clone project setup

---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Player.css'
+
+const Player = () => {
+  return (
+    <div className='player'> 
+      
+    </div>
+  )
+}
+ className='player'
+ export default Player
